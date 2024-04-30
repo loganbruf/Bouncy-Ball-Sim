0bb09bdf48a2a8b16102e779fc5c8de28ff6952c
Logan <74981154+loganbruf@users.noreply.github.com>
2024-04-30 17:52:43 +0300

    Added music note sound and trail.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEditor.Callbacks;
 using UnityEngine;
 
@@ -12,12 +13,32 @@ public class Ball : MonoBehaviour
 {
     public float speed;
     public float speedRangeModifier;
-    private Rigidbody2D rb;
+    public float musicPlayTime;
+    public List<Color> colors;
+    
+    public GameObject trail;
+    private AudioSource _audio;
+    private int _currColorIndex;
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sprite;
+    private float _timer;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _sprite = gameObject.GetComponent<SpriteRenderer>();
+        _audio = gameObject.GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+        trail.transform.localScale = gameObject.transform.localScale;
+        trail.GetComponent<SpriteRenderer>().color = colors[_currColorIndex];
+        Instantiate(trail, gameObject.transform.position, gameObject.transform.rotation);
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -26,7 +47,17 @@ public class Ball : MonoBehaviour
         float vecX, vecY;
         vecX = transform.position.x >= 0 ? -1 : 1;
         vecY = transform.position.y >= 0 ? -1 : 1;
-        rb.AddForce(new Vector2(vecX, vecY) * realSpeed);
+        _rb.AddForce(new Vector2(vecX, vecY) * realSpeed);
+        ChangeColor();
+        _timer += musicPlayTime;
+        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, 0) * 1.005f; //Ball get bigger.
+        _audio.Play();
+    }
+
+    void ChangeColor()
+    {
+        _currColorIndex = _currColorIndex + 1 >= colors.Count ? 0 : _currColorIndex + 1;
+        _sprite.color = colors[_currColorIndex];
     }
 
 }
